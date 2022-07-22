9db333746cab67f9ecccecb406890a818d82a35c
zackees <z@zackees.com>
2022-07-21 22:36:20 -0700

    fixes performance problem

--- secret/index-sample.js
+++ secret/index-sample.js
@@ -6,8 +6,8 @@ exports.rtcConfig = {
   iceServers: [
     {
       urls: [
-        'stun:stun.l.google.com:19302',
-        'stun:global.stun.twilio.com:3478'
+        'stun:global.stun.twilio.com:3478',
+        'stun:stun.l.google.com:19302' // Slow or timing out as of July 21, 2022
       ]
     },
     {
